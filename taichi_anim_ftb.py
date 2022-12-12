5ecfb275e7ea69af26b1960c32864ca51297b8be
Baiwenjiang <baiwen1979@qq.com>
2022-12-12 09:03:00 +0800

    添加注释

--- taichi_anim_ftb.py
+++ taichi_anim_ftb.py
@@ -7,6 +7,7 @@ n = 512
 # 画布的像素矩阵
 pixels = ti.Vector.field(3, ti.f32, shape=(n, n))
 
+# 核心渲染函数
 @ti.kernel
 def render(t: ti.f32):
     for y, x in pixels:
@@ -30,8 +31,10 @@ def render(t: ti.f32):
             c += th.frac(ti.sin(yb * 8 + xb * 42 + t * 1e-4 * 2 ** (levels - k - 1)) * 128) * weight
         pixels[y, x] = ti.Vector([1.0, 0.7, 0.8]) * c
 
+# 创建GUI窗口
 gui = ti.GUI('太极程序动画', res=(n, n))
 
+# 消息循环
 t = 0.0
 while gui.running:
     t += 0.1
